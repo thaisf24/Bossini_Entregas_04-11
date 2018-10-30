17ad34cf56fa494433c198bb337d88e3cd12acea
Thais <42677935+thaisf24@users.noreply.github.com>
2018-10-30 15:08:39 -0300

    Add files via upload

---
+++ ExerciciosBosssiniPatterns-master/src/Exercicio2_1/ExecutarNome.java
@@ -0,0 +1,15 @@
+package Exercicio2_1;
+
+public class ExecutarNome {
+
+	public static void main(String[] args) {
+		FormarNome fn = new FormarNome();
+		
+		String nome = "Henrique";
+		String sobrenome = "Lima";
+		
+		fn.getNome(nome, sobrenome);
+
+	}
+
+}
